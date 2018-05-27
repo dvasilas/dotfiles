79d6fe63a87afc6fea5c1c853bbd4e34bc8188a0
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2018-05-27 14:32:52 +0200

    installation

--- brew.sh
+++ brew.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+# Update to the latest Homebrew.
 brew update
 
+# Upgrade already-installed formulae.
 brew upgrade
 
-# Install GNU core utilities
+# Install GNU core utilities.
 brew install coreutils
 
-# Install some other useful utilities like `sponge`.
+# Install other useful utilities like `sponge`.
 brew install moreutils
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
@@ -17,9 +19,11 @@ brew install tree
 
 brew install gnu-sed --with-default-names
 
+# Install Bash 4.
 brew install bash
 brew install bash-completion2
 
+# Switch to brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
   chsh -s /usr/local/bin/bash;
@@ -30,4 +34,5 @@ brew install grep
 brew install openssh
 brew install screen
 
+# Remove outdated versions from the cellar.
 brew cleanup
